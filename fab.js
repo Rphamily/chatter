5abec49547f2474b9a1215cfb74432ad3ed3c0a8
Ryan Pham <64393944+Rphamily@users.noreply.github.com>
2025-03-25 01:16:54 -0400

    Update fab.js

--- fab.js
+++ fab.js
@@ -6,7 +6,7 @@ class Blur {
         this.div.id = "blurred_background";
         this.div.innerHTML = `<style>
         #blurred_background{
-            position:absolute;
+            position:fixed;
             top:0;
             left:0;
             height:100vh;
@@ -30,30 +30,36 @@ class Blur {
         oBody?.insertAdjacentElement("afterbegin", this.div);
     }
     close(){
-        this.div.remove();
+        this.div?.remove();
     }
 }
 
-
-const suffix = (Math.random()*100).toFixed().toString();
+const suffix = (Math.random() * 100).toFixed().toString();
 
 document.querySelector("body").insertAdjacentHTML("beforeend", `
     <style>
-    #fab${suffix}, #modal${suffix}{
-        position: absolute;
+    #fab${suffix}, #modal${suffix} {
+        position: fixed;
         bottom: 1em;
         right: 1em;
+        z-index: 1002;
     }
-    #clear${suffix}{
+    #clear${suffix} {
         position: absolute;
         top: -1em;
         right: 1em;
+        z-index: 1003;
     }
     </style>
     <button id="fab${suffix}">Tell me</button>
-    `);
+`);
 
-document.querySelector(`#fab${suffix}`).addEventListener("click", evt=>{
-    const blur = new Blur(`<div id="modal${suffix}"><div><x-chat /></div><button id="clear${suffix}">clear</button></div>`);
+document.querySelector(`#fab${suffix}`).addEventListener("click", () => {
+    const blur = new Blur(`
+        <div id="modal${suffix}">
+            <div><x-chat /></div>
+            <button id="clear${suffix}">Clear</button>
+        </div>
+    `);
     document.querySelector(`#clear${suffix}`).addEventListener("click", () => blur.close());
 });
