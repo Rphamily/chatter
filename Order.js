40148e55cb14deedc813c32001406e25cc28969e
Ryan Pham <64393944+Rphamily@users.noreply.github.com>
2025-03-25 01:00:19 -0400

    Update Order.js

--- Order.js
+++ Order.js
@@ -5,20 +5,30 @@ export class Order {
           let aReturn = [];
           this.stateCur = this.OrderState.RESERVING;
           aReturn.push("Welcome to Ryan's Viet Resto.");
-          aReturn.push("Would you like to order Pho, Bun Bo Hue, or Bun Rieu?");
+          aReturn.push("Would you like to order Pho, Bun Bo Hue, or Bun Rieu? ( Please type pho, bunbo, or bunrieu to reserve)");
           return aReturn;
         },
         RESERVING: (sInput) => {
           let aReturn = [];
           this.isDone = true;
-          if (sInput.toLowerCase().startsWith('y')) {
-            aReturn.push(`Your rapid test is reserved under the phone number ${this.sFrom}`);
+          if (sInput.toLowerCase().startsWith('pho')) {
+            aReturn.push(`Your Pho is reserved ${this.sFrom}`);
             let d = new Date();
             d.setMinutes(d.getMinutes() + 120);
-            aReturn.push(`Please pick it up at 123 Tidy St., Acton before ${d.toTimeString()}`);
+            aReturn.push(`Please pick it up at 123 Tidy St. at ${d.toTimeString()}`);
+          if (sInput.toLowerCase().startsWith('bunbo')) {
+            aReturn.push(`YourBun Bo Hue is reserved ${this.sFrom}`);
+            let d = new Date();
+            d.setMinutes(d.getMinutes() + 120);
+            aReturn.push(`Please pick it up at 123 Tidy St. at ${d.toTimeString()}`);
+          if (sInput.toLowerCase().startsWith('bunrieu')) {
+            aReturn.push(`Your Bun Rieu is reserved ${this.sFrom}`);
+            let d = new Date();
+            d.setMinutes(d.getMinutes() + 120);
+            aReturn.push(`Please pick it up at 123 Tidy St. at ${d.toTimeString()}`); 
           } else {
-            aReturn.push("Thanks for trying our reservation system");
-            aReturn.push("Maybe next time");
+            aReturn.push("Thank you!");
+            aReturn.push("Maybe next time you;ss decide to order :)");
           }
           return aReturn;
         }
